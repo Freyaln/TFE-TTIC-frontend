70692c81bcba74dfeb8d50c5f2d40f72d533e63d
Freyaln <freyling.aurelien@gmail.com>
2023-02-20 12:28:12 +0100

    FEAT:: allergies & diets forms front done.

---
+++ src/components/features/AllergiesForm.tsx
@@ -0,0 +1,110 @@
+import { FC } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Checkbox, Input } from '@mui/material';
+
+interface IAllergiesFormInput {
+    dairy: boolean;
+    egg: boolean;
+    gluten: boolean;
+    grain: boolean;
+    peanut: boolean;
+    seafood: boolean;
+    sesame: boolean;
+    shellfish: boolean;
+    soy: boolean;
+    sulfite: boolean;
+    tree_nut: boolean;
+    wheat: boolean;
+}
+const AllergiesForm: FC = ({}) => {
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            dairy: false,
+            egg: false,
+            gluten: false,
+            grain: false,
+            peanut: false,
+            seafood: false,
+            sesame: false,
+            shellfish: false,
+            soy: false,
+            sulfite: false,
+            tree_nut: false,
+            wheat: false,
+        },
+    });
+
+    const onSubmit: SubmitHandler<IAllergiesFormInput> = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                gap: '1rem',
+                maxHeight: '30rem',
+                flexWrap: 'wrap',
+            }}
+        >
+            <section>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="dairy" control={control} />
+                    <label>Dairy</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="egg" control={control} />
+                    <label>Egg</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="gluten" control={control} />
+                    <label>Gluten</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="grain" control={control} />
+                    <label>Grain</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="peanut" control={control} />
+                    <label>Peanut</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="seafood" control={control} />
+                    <label>Seafood</label>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="sesame" control={control} />
+                    <label>Sesame</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="shellfish" control={control} />
+                    <label>Shellfish</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="soy" control={control} />
+                    <label>Soy</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="sulfite" control={control} />
+                    <label>Sulfite</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="tree_nut" control={control} />
+                    <label>Tree nut</label>
+                </div>
+                <div>
+                    <Controller render={({ field }) => <Checkbox {...field} />} name="wheat" control={control} />
+                    <label>Wheat</label>
+                </div>
+            </section>
+            <Input type="submit" sx={{ alignSelf: 'flex-start' }} />
+        </form>
+    );
+};
+
+export default AllergiesForm;
---
+++ src/components/features/DietsForm.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Checkbox, Input } from '@mui/material';
+
+interface IDietsFormInput {
+    gluten_free: boolean;
+    vegetarian: boolean;
+    vegan: boolean;
+    pescetarian: boolean;
+    paleo: boolean;
+}
+const DietsForm: FC = ({}) => {
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            gluten_free: false,
+            vegetarian: false,
+            vegan: false,
+            pescetarian: false,
+            paleo: false,
+        },
+    });
+
+    const onSubmit: SubmitHandler<IDietsFormInput> = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                justifyContent: 'center',
+                gap: '1rem',
+                marginLeft: '10%',
+            }}
+        >
+            <div>
+                <Controller render={({ field }) => <Checkbox {...field} />} name="gluten_free" control={control} />
+                <label>Gluten free</label>
+            </div>
+            <div>
+                <Controller render={({ field }) => <Checkbox {...field} />} name="vegetarian" control={control} />
+                <label>Vegetarian</label>
+            </div>
+            <div>
+                <Controller render={({ field }) => <Checkbox {...field} />} name="vegan" control={control} />
+                <label>Vegan</label>
+            </div>
+            <div>
+                <Controller render={({ field }) => <Checkbox {...field} />} name="pescetarian" control={control} />
+                <label>Pescetarian</label>
+            </div>
+            <div>
+                <Controller render={({ field }) => <Checkbox {...field} />} name="paleo" control={control} />
+                <label>Paleo</label>
+            </div>
+            <Input type="submit" />
+        </form>
+    );
+};
+
+export default DietsForm;
