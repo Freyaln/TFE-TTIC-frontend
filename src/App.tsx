bc3c6ce2cd7f8704ac8d4ae636bdc30c9d0fcc1a
Freyaln <freyling.aurelien@gmail.com>
2023-02-23 16:17:19 +0100

    UPDATE:: Deleted mock datas (photos), api and redux slices

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import './App.css';
 import { configureStore } from '@reduxjs/toolkit';
-import { photosSlice } from './components/slices/recipesSlices';
-import { api } from './components/services/api.service';
 import Header from './components/features/Header';
 import { Outlet } from 'react-router';
+import { recipesSlice } from './components/slices/recipesSlices';
 
 export const store = configureStore({
     reducer: {
-        photos: photosSlice.reducer,
-        [api.reducerPath]: api.reducer,
+        recipe: recipesSlice.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
 
 function App() {
--- src/components/services/api.service.ts
+++
@@ -1,7 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
-    refetchOnMountOrArgChange: true,
-    endpoints: () => ({}),
-});
