13d6b9923c2740dbd9286341c642acdc71271799
Freyaln <freyling.aurelien@gmail.com>
2023-03-14 17:18:48 +0100

    FEAT:: Added updateReducer to store

--- src/components/utils/store.ts
+++ src/components/utils/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from '../slices/authSlices';
 import { recipeReducer } from '../slices/recipesSlices';
 import { registerReducer } from '../slices/registerSlices';
+import { updateReducer } from '../slices/updates.slices';
 
 export const store = configureStore({
     reducer: {
         recipe: recipeReducer,
         register: registerReducer,
         auth: authReducer,
+        update: updateReducer,
     },
 });
 
